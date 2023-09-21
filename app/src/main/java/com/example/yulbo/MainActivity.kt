6310ddb7188d50a288814ae33da86edd5b896054
no miu <mmieuxx@mius-MacBook-Air.local>
2023-09-21 11:17:21 +0900

    create start page

--- app/src/main/java/com/example/yulbo/MainActivity.kt
+++ app/src/main/java/com/example/yulbo/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.yulbo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.yulbo.ui.theme.YulboTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +47,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Yulbo",
+            modifier = modifier.padding(60.dp),
+            fontSize = 50.sp
+        )
+        Image(painter = painterResource(id = R.drawable.spacekoara), contentDescription = null,modifier = Modifier.padding(20.dp))
+        Button(
+            onClick = { /* Do something */ },
+            shape = MaterialTheme.shapes.small,
+            contentPadding = PaddingValues(top = 20.dp,bottom = 20.dp,start = 20.dp, end = 20.dp),
+            modifier = Modifier.padding(30.dp)
+        ){
+            Text(
+                text = "Start",
+                fontSize = 30.sp
+            )
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
