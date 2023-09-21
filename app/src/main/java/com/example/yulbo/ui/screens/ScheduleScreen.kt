a0ae05fb0e9f7006006654bafb1cbf517483c4cc
shizukanaoki <shizukanaoki14@gmail.com>
2023-09-21 14:34:38 +0900

    optimize import

--- app/src/main/java/com/example/yulbo/ui/screens/ScheduleScreen.kt
+++ app/src/main/java/com/example/yulbo/ui/screens/ScheduleScreen.kt
@@ -1,7 +1,6 @@
 package com.example.yulbo.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
