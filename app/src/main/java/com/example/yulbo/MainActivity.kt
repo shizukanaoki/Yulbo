17b33695fc437003c012cba607e946fd6338b5fa
shizukanaoki <shizukanaoki14@gmail.com>
2023-09-21 17:22:00 +0900

    optimize import

--- app/src/main/java/com/example/yulbo/MainActivity.kt
+++ app/src/main/java/com/example/yulbo/MainActivity.kt
@@ -3,21 +3,10 @@ package com.example.yulbo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.yulbo.ui.theme.YulboTheme
 
 class MainActivity : ComponentActivity() {
